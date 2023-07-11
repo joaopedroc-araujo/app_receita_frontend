2b87c1c44631082552964d99b35316c7b03d936c
Luiz Felipe dos Santos <lz18.felipe@gmail.com>
2023-07-10 23:34:37 -0300

    feat(7): resolve requisito 7, implementa o header

--- src/App.js
+++ src/App.js
@@ -3,16 +3,62 @@ import './App.css';
 // import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
 import Login from './pages/Login';
+import Meals from './pages/Meals';
+import Drinks from './pages/Drinks';
+// import Profile from './pages/Profile';
+// import Favorites from './pages/Favorites';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <Switch>
-        <Route path="/" exact component={ Login } />
-      </Switch>
-    </div>
+    <Switch>
+      <Route
+        path="/"
+        exact
+        component={ Login }
+      />
+      <Route
+        path="/meals"
+        render={ () => (
+          <>
+            <Header />
+            <Meals />
+          </>
+        ) }
+      />
+      <Route
+        path="/drinks"
+        render={ () => (
+          <>
+            <Header />
+            <Drinks />
+          </>
+        ) }
+      />
+      <Route path="/meals:id-da-receita" component={ Meals } />
+      <Route path="/drinks:id-da-receita" component={ Drinks } />
+      <Route path="/meals:id-da-receita/in-progress" component={ Meals } />
+      <Route path="/drinks:id-da-receita/in-progress" component={ Drinks } />
+      <Route
+        path="/profile"
+        render={ () => (
+          <Header />
+        ) }
+      />
+      <Route
+        path="/done-recipes"
+        render={ () => (
+          <Header />
+        ) }
+      />
+      <Route
+        path="/favorite-recipes"
+        render={ () => (
+          <Header />
+        ) }
+      />
+    </Switch>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,53 @@
-function Header() {
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import '../styles/Header.css';
 
+import logo from '../images/logo.svg';
+import searchIcon from '../images/searchIcon.svg';
+import profileIcon from '../images/profileIcon.svg';
+
+function Header() {
+  const location = useLocation();
+  const showSearchIcon = ['/meals', '/drinks'].includes(location.pathname);
+
+  const getPageTitle = (pathname) => {
+    switch (pathname) {
+    case '/meals':
+      return 'Meals';
+    case '/drinks':
+      return 'Drinks';
+    case '/profile':
+      return 'Profile';
+    case '/done-recipes':
+      return 'Done Recipes';
+    case '/favorite-recipes':
+      return 'Favorite Recipes';
+    default:
+      return '';
+    }
+  };
+
+  return (
+    <header>
+      <div className="title-container">
+        <img src={ logo } alt="logo" />
+        <h2 data-testid="page-title">
+          {getPageTitle(location.pathname)}
+        </h2>
+      </div>
+
+      <div className="header-btns">
+        {showSearchIcon && (
+          <Link to="/search">
+            <img src={ searchIcon } alt="Search" data-testid="search-top-btn" />
+          </Link>
+        )}
+        <Link to="/profile">
+          <img src={ profileIcon } alt="Profile" data-testid="profile-top-btn" />
+        </Link>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
