83d2a170a7e8888cffcbb37c2053b056bb12634a
Vinicius Santos de Araujo <96455030+vini2932@users.noreply.github.com>
2023-07-11 22:49:23 -0300

    Fix: Resolve erro do lint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-// import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Login from './pages/Login';
-import SearchBar from './components/SearchBar';
 import Footer from './components/Footer';
 import Drinks from './components/Drinks';
 import Meals from './components/Meals';
